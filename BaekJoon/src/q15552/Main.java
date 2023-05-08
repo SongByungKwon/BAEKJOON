dc604da9457ce1c670e0ffbf40f79b841e4fc468
Class01 <Class01@DESKTOP-46EHI55>
2023-05-08 16:55:52 +0900

    baekjoon

---
+++ BaekJoon/src/q15552/Main.java
@@ -0,0 +1,20 @@
+package q15552;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String s1 =st.nextToken();
+		String s2 =st.nextToken();
+		
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}
--- BaekJoon/src/q2439/Main.java
+++ BaekJoon/src/q2439/Main.java
@@ -1,4 +1,4 @@
-package q2438;
+package q2439;
 
 import java.util.Scanner;
 
@@ -8,25 +8,17 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		sc.close();
-//		String st= "*";
-//		for(int i=0; i<s;i++) {
-//			System.out.println(st);
-//			st=st+"*";
-//		}
-//
-//	}
-//
-//}
+
 		
-		
-		
-		System.out.println(N);
 		for(int i=0;i<N;i++) {
-			for(int j=0;j<=i;j++) {
+			
+			for(int j=1;j<N-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<=i;k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 			}
 		}
-		
